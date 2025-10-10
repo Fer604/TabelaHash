421c80a8cae5113ff3c0c802fa22d20e8db6d1b7
Fer604 <fernando.as604@gmail.com>
2025-10-10 14:11:46 -0300

    definição do q uma tabela precisa

--- src/hashTable.java
+++ src/hashTable.java
@@ -1,2 +1,18 @@
-public class hashTable {
+public interface hashTable {
+    //interface base(os os metodos que uma tabela PRECISA ter)
+
+    void inserir(registro r);
+
+    boolean buscar(registro r); // como pela especificação do trabalho "implementar e analisar o desempenho de diferentes tabelas hash em Java." só nos interessa se o registro existe ou não para a analise de desempenho logo não retorna o registro e sim boolean
+
+    int getColisoes();
+
+    int getTamanho();
+
+    int getElementos(); //qtd total de elementos aramazenados
+
+    double getFatorCarga();//retorna o fator de carga (load factor = elementos / tamanho da tabela).
+
+    void limpar();//sai sujeira
+
 }
--- src/registro.java
+++ src/registro.java
@@ -1,2 +1,18 @@
 public class registro {
+    private final String codigo;
+
+    public registro(String codigo) {
+        this.codigo = codigo;
+    }
+    public String getCodigo() {
+        return codigo;
+    }
+    public int getCodigoNumerico() {
+        return Integer.parseInt(codigo);
+    }
+
+    @Override
+    public String toString() {
+        return codigo;
+    }
 }
