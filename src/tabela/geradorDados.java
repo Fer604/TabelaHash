053abb1ec44bbf39cf8d9f1b9a29c7b4e53eb4a0
andrééé <andreestevesarantes19@gmail.com>
2025-10-15 20:02:08 -0300

    testando ainda, mas ta aqui

--- src/tabela/geradorDados.java
+++ src/tabela/geradorDados.java
@@ -1,13 +1,36 @@
 package tabela;
+
 import java.util.Random;
+import java.util.function.Consumer;
+
+/** Seeded data generator for registro objects. */
 public class geradorDados {
-    public static registro[] gerar(long seed,int tamanho){
-        Random r = new Random(seed);
+    private final Random rnd;
+
+    public geradorDados(long seed) {
+        this.rnd = new Random(seed);
+    }
+
+    /** Generate an array of registros */
+    public registro[] gerar(int tamanho) {
         registro[] dados = new registro[tamanho];
-        for (int i=0;i<tamanho;i++){
-            int num =  r.nextInt(999999999);
+        for (int i = 0; i < tamanho; i++) {
+            int num = rnd.nextInt(999999999);
             dados[i] = new registro(Integer.toString(num));
         }
         return dados;
+    }
+
+    /** Insert all registros into a hashTable and return elapsed ms */
+    public long inserirTodos(hashTable tabela, registro[] dados) {
+        long inicio = System.currentTimeMillis();
+        for (registro r : dados) tabela.inserir(r);
+        long fim = System.currentTimeMillis();
+        return fim - inicio;
+    }
+
+    /** Allow custom processing for each registro */
+    public void forEachRegistro(registro[] dados, Consumer<registro> consumer) {
+        for (registro r : dados) consumer.accept(r);
     }
 }
